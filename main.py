d10d3369cc400489535ffaf30a1075aaaa070740
agkthen <113641583+Ashton-Gates@users.noreply.github.com>
2023-08-18 08:37:16 -0400

    Update main.py
    Added ASCII and more efficient option flows.

--- main.py
+++ main.py
@@ -3,61 +3,78 @@ from socketserver import TCPServer
 import ssl
 import socket
 
+class Main:
+    def __init__(self):
+        self.show_welcome() 
 
+    def show_welcome(self):
+        print("""
+ ___           ___      ________       ___  __        ___              
+|\  \         |\  \    |\   ___ \    |\  \|\  \     |\  \             
+\ \  \        \ \  \   \ \  \\ \  \   \ \  \/  /|_   \ \  \            
+ \ \  \        \ \  \   \ \  \\ \  \   \ \   ___  \   \ \  \           
+  \ \  \____    \ \  \   \ \  \\ \  \   \ \  \\ \  \   \ \  \____      
+   \ \_______\   \ \__\   \ \__\\ \__\   \ \__\\ \__\   \ \_______\    
+    \|_______|    \|__|    \|__| \|__|   \|__| \|__|    \|_______|    
+                                                                       
+                                                                       
+                                                                       
+  """)
+        print("Welcome to the most user-frienly web server: LINKL! We aimed to make this as usable and efficient as possible.")
+        
+        input("Press Enter to continue...")  # Wait for user to press Enter
 
+        self.choose_option()
+        
+    # Prompts the user to input the server address and web directory
+    def choose_option(self):
+        choice = input("Choose an option: 1 (port 8080) or 2 (port 443): ")
+        if choice == '1':
+            self.option_8080()
+        elif choice == '2':
+            self.option_443()
+        else:
+            print("Invalid option. Exiting.")
+    
+    def option_8080(self):
+        server_address = self.get_server_address()  
+        custom_directory = self.get_web_directory()
+        httpd = TCPServer((server_address, 8080), CustomHandler)
+        httpd.custom_directory = custom_directory
+        httpd.web_root = httpd.RequestHandlerClass.translate_path(httpd, '/')
+        print(f"Server running on http://{server_address}:8080")
+        httpd.serve_forever()
 
-def get_server_address():
-    user_input = input("Enter a registered DNS name or IP address: ")
-    try:
-        ip_address = socket.gethostbyname(user_input)
-        print(f"Resolved {user_input} to {ip_address}")
-    except socket.gaierror:
-        ip_address = user_input
-        print(f"Using IP address {ip_address}")
-    return ip_address
-
-def get_web_directory():
-    directory_path = input("Enter the path to the directory containing the web files: ")
-    return directory_path
-
+    def option_443(self):
+        port = 443
+        certfile = input("Enter the path to the certificate file: ")
+        keyfile = input("Enter the path to the key file: ")
+        handler = SimpleHTTPRequestHandler
+        server_address = ('', port)
+        httpd = linkl(server_address, handler, certfile, keyfile)
+        print(f"Server running on port {port}")
+        httpd.serve_forever()
+    
+    def get_server_address(self):
+        user_input = input("Enter a registered DNS name or IP address: ")
+        try:
+            ip_address = socket.gethostbyname(user_input)
+            print(f"Resolved {user_input} to {ip_address}")
+        except socket.gaierror:
+            ip_address = user_input
+            print(f"Using IP address {ip_address}")
+        return ip_address
+    
+    def get_web_directory(self):
+        directory_path = input("Enter the path to the directory containing the web files: ")
+        return directory_path
+    
 class CustomHandler(SimpleHTTPRequestHandler):
     def translate_path(self, path):
         path = super().translate_path(path)
         return path.replace(self.server.web_root, self.server.custom_directory)
 
-def main():
-    # ASCII Art Placeholder
-    print("ASCII ART HERE")
-
-    # Prompt the user for the server address and web directory
-    server_address = get_server_address()
-    custom_directory = get_web_directory()
-
-    # Create the server
-    httpd = TCPServer((server_address, 8080), CustomHandler)
-
-    # Set custom attributes for the server
-    httpd.custom_directory = custom_directory
-    httpd.web_root = httpd.RequestHandlerClass.translate_path(httpd, '/')
-
-    # Configure TLS/SSL
-    httpd.socket = ssl.wrap_socket(httpd.socket,
-                                   certfile='cert.pem',
-                                   keyfile='key.pem',
-                                   server_side=True)
-
-    print(f"Server running on https://{server_address}:8080")
-    httpd.serve_forever()
-    
-def get_port_and_ssl():
-    port = input("Choose the port (443 for SSL or 8080 without SSL): ")
-    certfile = keyfile = None
-    if port == '443':
-        certfile = input("Enter the path to the certificate file: ")
-        keyfile = input("Enter the path to the key file: ")
-    return int(port), certfile, keyfile
-
-class MyServer(TCPServer):
+class linkl(TCPServer):
     def __init__(self, address, handler, certfile=None, keyfile=None):
         super().__init__(address, handler)
         if certfile and keyfile:
@@ -65,12 +82,5 @@ class MyServer(TCPServer):
             context.load_cert_chain(certfile, keyfile)
             self.socket = context.wrap_socket(self.socket, server_side=True)
 
-port, certfile, keyfile = get_port_and_ssl()
-handler = SimpleHTTPRequestHandler
-server_address = ('', port)
-httpd = MyServer(server_address, handler, certfile, keyfile)
-print(f"Server running on port {port}")
-httpd.serve_forever()
-
 if __name__ == "__main__":
-    main()
+    Main()
